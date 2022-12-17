567040a7923a4c877e8167d05a0c66cc1bb9be18
Velnbur <kirillbaybula@gmail.com>
2022-12-17 21:44:33 +0200

    Remove copus

--- internal/config/main.go
+++ internal/config/main.go
@@ -4,14 +4,11 @@ import (
 	"github.com/Velnbur/uniswapv2-indexer/internal/contracts"
 	"github.com/go-redis/redis/v8"
 	"gitlab.com/distributed_lab/kit/comfig"
-	"gitlab.com/distributed_lab/kit/copus"
-	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/kit/kv"
 )
 
 type Config interface {
 	comfig.Logger
-	types.Copuser
 	comfig.Listenerer
 
 	Contracter
@@ -24,7 +21,6 @@ type Config interface {
 
 type config struct {
 	comfig.Logger
-	types.Copuser
 	comfig.Listenerer
 	getter kv.Getter
 
@@ -39,7 +35,6 @@ type config struct {
 func New(getter kv.Getter) Config {
 	return &config{
 		getter:     getter,
-		Copuser:    copus.NewCopuser(getter),
 		Listenerer: comfig.NewListenerer(getter),
 		Logger:     comfig.NewLogger(getter, comfig.LoggerOpts{}),
 		Contracter: NewContracterCfg(getter),
--- internal/service/api/main.go
+++ internal/service/api/main.go
@@ -4,34 +4,29 @@ import (
 	"context"
 	"net"
 
-	"github.com/Velnbur/uniswapv2-indexer/internal/config"
 	"github.com/go-chi/chi"
 	"gitlab.com/distributed_lab/ape"
-	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/logan/v3"
-	"gitlab.com/distributed_lab/logan/v3/errors"
+
+	"github.com/Velnbur/uniswapv2-indexer/internal/config"
 )
 
 type service struct {
 	log      *logan.Entry
-	copus    types.Copus
 	listener net.Listener
 	router   chi.Router
+	cfg      config.Config
 }
 
-func (s *service) run(ctx context.Context, cfg config.Config) error {
-	if err := s.copus.RegisterChi(s.router); err != nil {
-		return errors.Wrap(err, "cop failed")
-	}
-
-	ape.Serve(ctx, s.router, cfg, ape.ServeOpts{})
+func (s *service) run(ctx context.Context) error {
+	ape.Serve(ctx, s.router, s.cfg, ape.ServeOpts{})
 	return nil
 }
 
 func newService(cfg config.Config) *service {
 	return &service{
+		cfg:      cfg,
 		log:      cfg.Log(),
-		copus:    cfg.Copus(),
 		listener: cfg.Listener(),
 		router:   newRouter(cfg),
 	}
