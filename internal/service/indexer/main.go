834c648f9ce658c72462f59c002951c621fe3df8
Velnbur <kirillbaybula@gmail.com>
2022-12-17 21:37:27 +0200

    Add graph dumping at graceful shutdown

--- internal/service/indexer/main.go
+++ internal/service/indexer/main.go
@@ -2,9 +2,11 @@ package indexer
 
 import (
 	"context"
+	"time"
 
 	"github.com/Velnbur/uniswapv2-indexer/internal/channels"
 	"github.com/Velnbur/uniswapv2-indexer/internal/config"
+	"github.com/Velnbur/uniswapv2-indexer/internal/providers"
 	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
 )
@@ -14,6 +16,7 @@ type Indexer struct {
 
 	logger      *logan.Entry
 	eventsQueue channels.EventQueue
+	pathes      providers.PathesProvider
 }
 
 func New(cfg config.Config) *Indexer {
@@ -21,8 +24,11 @@ func New(cfg config.Config) *Indexer {
 		graph:       NewGraph(),
 		eventsQueue: cfg.EventsQueue(),
 		logger:      cfg.Log(),
+		pathes:      providers.NewPathesRedisProvider(cfg.Redis()),
 	}
 }
+
+const defaultDumpTimeout = time.Second * 5
 
 func (ind *Indexer) Run(ctx context.Context) error {
 	eventsSubscription, err := ind.eventsQueue.Receive(ctx)
@@ -33,6 +39,7 @@ func (ind *Indexer) Run(ctx context.Context) error {
 	for {
 		select {
 		case <-ctx.Done():
+			ind.dumpGraphWithTimeout()
 			return nil
 		case event := <-eventsSubscription:
 			ind.processEvent(ctx, &event)
@@ -59,4 +66,33 @@ func (ind *Indexer) processEvent(ctx context.Context, event *channels.Event) {
 			event.PairCreation.Reserve1,
 		)
 	}
+}
+
+func (ind *Indexer) dumpGraphWithTimeout() {
+	ctxWithTimeout, cancel := context.WithTimeout(context.Background(), defaultDumpTimeout)
+
+	go func() {
+		if err := ind.dumpGraph(ctxWithTimeout); err != nil {
+			ind.logger.WithError(err).Error("failed to dump graph")
+		}
+	}()
+
+	select {
+	case <-ctxWithTimeout.Done():
+		cancel()
+	}
+}
+
+func (ind *Indexer) dumpGraph(ctx context.Context) error {
+	for edge, pathes := range ind.graph.pathesMap.m {
+		err := ind.pathes.SetPathes(ctx, edge.Token0, edge.Token1, pathes)
+		if err != nil {
+			return errors.Wrap(err, "failed to dump pathes", logan.F{
+				"token0": edge.Token0.Hex(),
+				"token1": edge.Token1.Hex(),
+			})
+		}
+	}
+
+	return nil
 }
